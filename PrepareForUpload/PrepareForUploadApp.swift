8b092e0a98356b89469359fee512922c90999bf9
Ludovic HENRY <badsteam@hotmail.com>
2023-06-29 15:33:09 -0400

    Initial Commit

---
+++ PrepareForUpload/PrepareForUploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrepareForUploadApp.swift
+//  PrepareForUpload
+//
+//  Created by Ludovic HENRY on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct PrepareForUploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
