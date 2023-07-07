02a3bc7622d64369cf9a97d8f403c314105170e8
Ludovic HENRY <badsteam@hotmail.com>
2023-07-07 13:16:34 -0400

    All the logic is placed in the viewModel

--- PrepareForUpload/View/Components/ImageToCrop.swift
+++ PrepareForUpload/View/Components/ImageToCrop.swift
@@ -9,75 +9,33 @@ import SwiftUI
 
 struct ImageToCrop: View {
     @ObservedObject var viewModel: CropViewModel
-    @State var isInteracting: Bool = false
-    
-    var body: some View {
-        ImageView()
-            .frame (maxWidth: .infinity, maxHeight: .infinity)
-    }
 
-    @ViewBuilder func ImageView() -> some View {
-            if let photo = viewModel.photo {
-                photo
-                    .resizable()
-                    .scaledToFill()
-                    .overlay {
-                        GeometryReader { geometry in
-                            Color.clear
-                                .onChange(of: isInteracting) { newValue in
-                                    withAnimation(.spring()) {
-                                        viewModel.fixPosition(size: geometry.size)
-                                    }
-                                }
-                        }
-                    }
-                    .frame(width: CropViewModel.frameWidth, height: CropViewModel.frameheight, alignment: .center)
-                    .scaleEffect(viewModel.imageScale * viewModel.lastScale)
-                    .offset(viewModel.imageOffset)
-                    .clipped()
-                // MARK: - Drag Gesture
-                    .gesture(
-                        DragGesture()
-                            .onChanged{ value in
-                                isInteracting = true
-                                viewModel.dragOffset = value.translation
-                            }.onEnded{ value in
-                                viewModel.position.width += value.translation.width
-                                viewModel.position.height += value.translation.height
-                                viewModel.dragOffset = .zero
-                                isInteracting = false
-                            }
-                    )
-                // MARK: - Magnification Gesture
-                    .gesture(
-                        MagnificationGesture()
-                            .onChanged{ value in
-                                    isInteracting = true
-                                    viewModel.lastScale = value
-                            }
-                            .onEnded{ value in
-                                viewModel.imageScale *= value
-                                viewModel.lastScale = 1
+    var body: some View {
+        if let photo = viewModel.photo {
+            photo
+                .resizable()
+                .scaledToFill()
+                .overlay {
+                    GeometryReader { geometry in
+                        Color.clear
+                            .onChange(of: viewModel.isInteracting) { newValue in
                                 withAnimation(.spring()) {
-                                    if viewModel.imageScale > 3 {
-                                        viewModel.imageScale = 3
-                                    } else if viewModel.imageScale < 1 {
-                                        viewModel.imageScale = 1
-                                    }
+                                    viewModel.fixPosition(size: geometry.size)
                                 }
-                                isInteracting = false
                             }
-                    )
-                // MARK: - Double tap gesture
-                    .onTapGesture(count: 2) {
-                        withAnimation(.spring()) {
-                            viewModel.imageScale = 1
-                            viewModel.position = .zero
-                        }
                     }
-            }
+                }
+                .frame(width: CropViewModel.frameWidth, height: CropViewModel.frameheight, alignment: .center)
+                .scaleEffect(viewModel.imageScale * viewModel.lastScale)
+                .offset(viewModel.imageOffset)
+                .clipped()
+                .gesture(viewModel.dragGesture())
+                .gesture(viewModel.magnificationGesture())
+                .onTapGesture(count: 2) {viewModel.tapGesture()}
+        }
     }
 }
+
 
 struct ImageToCrop_Previews: PreviewProvider {
     static var previews: some View {
--- PrepareForUpload/ViewModel/CropViewModel.swift
+++ PrepareForUpload/ViewModel/CropViewModel.swift
@@ -19,7 +19,7 @@ class CropViewModel: ObservableObject {
 
     @Published var cropedImage: UIImage?
     @Published var selectedItem: PhotosPickerItem? {
-        didSet { loadThePhoto() }
+        didSet { if selectedItem != nil { loadThePhoto() } }
     }
     @Published var photo: Image?
     @Published var dragOffset: CGSize = .zero
@@ -27,6 +27,7 @@ class CropViewModel: ObservableObject {
     @Published var imageScale: CGFloat = 1
     @Published var lastScale: CGFloat = 1
     @Published var isPresentedCropView: Bool = false
+    @Published var isInteracting: Bool = false
 
     init(photo: Image? = nil) {
         if let photo { self.photo = photo }
@@ -37,19 +38,60 @@ class CropViewModel: ObservableObject {
         dragOffset = .zero
         position = .zero
         imageScale = 1
-        selectedItem = nil
     }
 
     func loadThePhoto() {
         Task {
-            guard let data = try? await selectedItem?.loadTransferable(type: Data.self) else { return print("ERROR")}
-            guard let uiImage = UIImage(data: data) else { return print("ERROR") }
+            guard let selectedItem else { return print("Not Selected")}
+            guard let data = try? await selectedItem.loadTransferable(type: Data.self) else { return print("ERROR1")}
+            guard let uiImage = UIImage(data: data) else { return print("ERROR2") }
             await MainActor.run {
                 photo = Image(uiImage: uiImage)
                 isPresentedCropView = true
+                self.selectedItem = nil
             }
             return
         }
+    }
+
+    func tapGesture() {
+        withAnimation(.spring()) {
+            imageScale = 1
+            position = .zero
+        }
+    }
+
+    func dragGesture() -> some Gesture {
+        DragGesture()
+            .onChanged{ value in
+                self.isInteracting = true
+                self.dragOffset = value.translation
+            }.onEnded{ value in
+                self.position.width += value.translation.width
+                self.position.height += value.translation.height
+                self.dragOffset = .zero
+                self.isInteracting = false
+            }
+    }
+
+    func magnificationGesture() -> some Gesture {
+        MagnificationGesture()
+            .onChanged{ value in
+                self.isInteracting = true
+                self.lastScale = value
+            }
+            .onEnded{ value in
+                self.imageScale *= value
+                self.lastScale = 1
+                withAnimation(.spring()) {
+                    if self.imageScale > 3 {
+                        self.imageScale = 3
+                    } else if self.imageScale < 1 {
+                        self.imageScale = 1
+                    }
+                }
+                self.isInteracting = false
+            }
     }
 
     func fixPosition(size: CGSize) {
@@ -68,6 +110,4 @@ class CropViewModel: ObservableObject {
             self.position.height = -heightLimit
         }
     }
-
-
 }
